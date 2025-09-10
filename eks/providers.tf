19bc7aab169614c7dc46765d23bd77d4273a7063
ChongZhe001025 <czhuang001025@gmail.com>
2025-09-10 11:32:19 +0800

    fix(providers): remove unsupported load_config_file attribute for kubernetes/helm provider compatibility

--- eks/providers.tf
+++ eks/providers.tf
@@ -3,26 +3,20 @@ provider "aws" {
   profile = var.aws_profile
 }
 
-data "aws_eks_cluster" "this" {
-  name = var.cluster_name
-  depends_on = [module.eks]
-}
-
 data "aws_eks_cluster_auth" "this" {
-  name = var.cluster_name
-  depends_on = [module.eks]
+  name = module.eks.cluster_name
 }
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   token                  = data.aws_eks_cluster_auth.this.token
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.this.token
   }
 }
